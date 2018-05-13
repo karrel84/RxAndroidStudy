beb3a85cac4de270d519e3068307f4870c9c46b0
noah.kim <babosamo@gmail.com>
2018-05-13 23:59:54 +0900

    reduce 테스트. maybe 를 써야 하는이유는?

---
+++ Chapter03/ReduceTestSample/app/src/test/java/com/babosamo/chapter03/ExampleUnitTest.kt
@@ -0,0 +1,55 @@
+package com.babosamo.chapter03
+
+import io.reactivex.Maybe
+import io.reactivex.Observable
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun ball1() {
+        val balls = listOf<String>("a", "b", "c")
+        val source: Maybe<String> = Observable.fromIterable(balls).reduce { value1, value2 -> "$value2($value1)" }
+        source.subscribe{result -> assertEquals("c(b(a))", result);
+        System.out.println(result)}
+    }
+
+    @Test
+    fun ball2() {
+        val balls = listOf<String>()
+        val source: Maybe<String> = Observable.fromIterable(balls).reduce { value1, value2 -> "$value2($value1)" }
+        source.subscribe{result -> assertEquals("c(b(a))", result);
+            System.out.println(result)}
+    }
+
+
+    private val mergeBall : ((ball1: String, ball2:String ) -> String) = {
+        value1, value2 -> "$value2($value1)"
+    }
+
+    @Test
+    fun ball3() {
+//        val arrayData = arrayOf("a", "b", "c") // fixme ㅠㅠ
+        val source: Maybe<String> = Observable.fromArray("a","b","c").reduce (mergeBall)
+        source.subscribe{result -> assertEquals("c(b(a))", result)}
+    }
+
+
+    @Test
+    fun ball4() {
+        val source: Maybe<String> = Observable.fromArray("a").reduce {value1 , value2 ->  value2}
+        source.subscribe{result -> assertEquals("a", result)}
+    }
+
+
+}
